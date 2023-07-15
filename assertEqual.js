ba4182f789a95287253bf6b2e57fbf36cb8acffb
Mia Dinh <miadinh@Mias-MacBook-Air.local>
2023-07-15 12:34:25 -0400

    Implement assertEqual with emoji

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// function implementation
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ " + "Assertion Passed: " + [actual] + " === " + [expected]);
+  } else {
+    console.log("🛑🛑🛑 " + "Assertion Failed: " + [actual] + " !== " + [expected]);
+  }
+};
+
+// test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+
