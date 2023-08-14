3a9632f9e2f0b365d8c0d72a947700d558392d61
Mia Dinh <miadinh38@gmail.com>
2023-08-14 01:00:31 -0400

    Added assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,34 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
-  for (const key of Object.keys(object1)) {
-    const value1 = object1[key];
-    const value2 = object2[key];
-    if (Array.isArray(value1) && Array.isArray(value2)) { //check if value is array and pass to eqArrays function
-      if (!eqArrays(value1,value2)) {
-        return false;
-      }
-    } else if (value1 !== value2) { //if they are primitives, compare 2 values
-      return false;
-    }
-  }
-  return true;
-};
-
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -39,9 +9,6 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-const test1 = { a: '1', b: 2 };
-const test2 = { b: 2, a: '1' };
-assertObjectsEqual(test1, test2);
-
+module.exports = assertObjectsEqual;
 
 
---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+describe('assertObjectsEqual', function() {
+  it('should pass for equal objects', function() {
+    const test1 = { a: '1', b: 2 };
+    const test2 = { b: 2, a: '1' };
+    assertObjectsEqual(test1, test2);
+  });
+
+  it('should fail for non-equal objects', function() {
+    const test1 = { a: '1', b: 2 };
+    const test2 = { a: '1', b: 3 };
+    assertObjectsEqual(test1, test2);
+  });
+});
+
